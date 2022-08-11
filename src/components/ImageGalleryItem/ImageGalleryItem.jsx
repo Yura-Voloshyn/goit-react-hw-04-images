f092cfa80e5409fd40a494662180450a168559bf
Yura-Voloshyn <yuriy_voloshyn@icloud.com>
2022-08-11 17:40:09 +0300

    start

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,33 +1,51 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import { StyledLi, StyledItemImage } from './ImageGalleryItem.styled';
 import Modal from '../Modal';
 import PropTypes from 'prop-types';
-class GalleyItem extends Component {
-  state = {
-    showModal: false,
+
+const GalleyItem = ({ tags, webformatURL, largeImageURL }) => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(showModal => ({ showModal: !showModal }));
   };
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-  render() {
-    const { tags, webformatURL, largeImageURL } = this.props;
-    const { showModal } = this.state;
-    return (
-      <StyledLi>
-        <StyledItemImage
-          onClick={this.toggleModal}
-          src={webformatURL}
-          alt={tags}
-        />
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt={tags} />
-          </Modal>
-        )}
-      </StyledLi>
-    );
-  }
-}
+  return (
+    <StyledLi>
+      <StyledItemImage onClick={toggleModal} src={webformatURL} alt={tags} />
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt={tags} />
+        </Modal>
+      )}
+    </StyledLi>
+  );
+};
+
+// class GalleyItem1 extends Component {
+//   state = {
+//     showModal: false,
+//   };
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({ showModal: !showModal }));
+//   };
+//   render() {
+//     const { tags, webformatURL, largeImageURL } = this.props;
+//     const { showModal } = this.state;
+//     return (
+//       <StyledLi>
+//         <StyledItemImage
+//           onClick={this.toggleModal}
+//           src={webformatURL}
+//           alt={tags}
+//         />
+//         {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <img src={largeImageURL} alt={tags} />
+//           </Modal>
+//         )}
+//       </StyledLi>
+//     );
+//   }
+// }
 GalleyItem.propTypes = {
   tags: PropTypes.string,
   webformatURL: PropTypes.string.isRequired,
